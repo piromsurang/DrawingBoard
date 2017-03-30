6a1e7a4af352ab5b4150bdeeb4bd593bb915284b
Piromsurang <Piromsurang@Piromsurangs-MacBook-Pro.local>
2017-03-30 18:11:48 +0900

    finish fix bugwhen grouped delete labels

---
+++ src/main/DrawingBoard.java
@@ -0,0 +1,122 @@
+package main;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JPanel;
+import objects.*;
+
+public class DrawingBoard extends JPanel {
+
+	private MouseAdapter mouseAdapter; 
+	private List<GObject> gObjects;
+	private GObject target;
+	
+	private int gridSize = 10;
+	
+	public DrawingBoard() {
+		gObjects = new ArrayList<GObject>();
+		mouseAdapter = new MAdapter();
+		addMouseListener(mouseAdapter);
+		addMouseMotionListener(mouseAdapter);
+		setPreferredSize(new Dimension(800, 600));
+	}
+	
+	public void addGObject(GObject gObject) {
+		gObjects.add( gObject );
+		repaint();
+	}
+	
+	public void groupAll() {
+		CompositeGObject objects = new CompositeGObject();
+		for( GObject g : gObjects ) {
+			g.deselected();
+			objects.add(g);
+		}
+		objects.recalculateRegion();
+		clear();
+		gObjects.add( objects );
+		repaint();
+	}
+
+	public void deleteSelected() {
+		gObjects.remove( target );
+		repaint();
+	}
+	
+	public void clear() {
+		gObjects.clear();
+		repaint();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		paintBackground(g);
+		paintGrids(g);
+		paintObjects(g);
+	}
+
+	private void paintBackground(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(0, 0, getWidth(), getHeight());
+	}
+
+	private void paintGrids(Graphics g) {
+		g.setColor(Color.lightGray);
+		int gridCountX = getWidth() / gridSize;
+		int gridCountY = getHeight() / gridSize;
+		for (int i = 0; i < gridCountX; i++) {
+			g.drawLine(gridSize * i, 0, gridSize * i, getHeight());
+		}
+		for (int i = 0; i < gridCountY; i++) {
+			g.drawLine(0, gridSize * i, getWidth(), gridSize * i);
+		}
+	}
+
+	private void paintObjects(Graphics g) {
+		for (GObject go : gObjects) {
+			go.paint(g);
+		}
+	}
+
+	class MAdapter extends MouseAdapter {
+
+		int x;
+		int y;
+		
+		private void deselectAll() {
+			for( GObject g : gObjects ) {
+				g.deselected();
+			}
+			repaint();
+		}
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			x = e.getX();
+			y = e.getY();
+			deselectAll();
+			for( GObject g : gObjects ) {
+				if( g.pointerHit(x, y)) {
+					g.selected();
+					target = g;
+					break;
+				} 
+			}
+			repaint();
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			x = e.getX();
+			y = e.getY();
+			
+			target.move(x, y);
+			
+			repaint();
+		}
+	}
+	
+}
---
+++ src/objects/CompositeGObject.java
@@ -0,0 +1,72 @@
+package objects;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeGObject extends GObject {
+
+	private List<GObject> gObjects;
+
+	public CompositeGObject() {
+		super(0, 0, 0, 0);
+		gObjects = new ArrayList<GObject>();
+	}
+
+	public void add(GObject gObject) {
+		gObjects.add( gObject );
+	}
+
+	public void remove(GObject gObject) {
+		gObjects.remove( gObject );
+	}
+
+	@Override
+	public void move(int dX, int dY) {
+		int x = super.x;
+		int y = super.y;
+		super.move(dX, dY);
+		for( GObject g : gObjects ) {
+			g.move( dX - x + g.x, dY - y + g.y );
+		}
+	}
+	
+	public void recalculateRegion() {
+		int minx = 400;
+		int miny = 400;
+		int maxx = 0;
+		int maxy = 0;
+		int heightmax = 0;
+		int widthmax = 0;
+		for( GObject g : gObjects ) {
+			if( minx > g.x ) minx = g.x;
+			if( miny > g.y ) miny = g.y;
+			if( maxx < g.x ) {
+				maxx = g.x;
+				heightmax = g.height;
+			}
+			if( maxy < g.y ) {
+				maxy = g.y;
+				widthmax = g.width;
+			}
+		}
+		
+		super.x = minx;
+		super.y = miny;
+		super.height = maxy - miny + heightmax;
+		super.width = maxx - minx + widthmax;
+	}
+
+	@Override 
+	public void paintObject(Graphics g) {
+		for( GObject a : gObjects ) 
+			a.paintObject(g);
+	}
+
+	@Override
+	public void paintLabel(Graphics g) {
+		g.drawString("Grouped", x, y+height+10);
+	}
+	
+}
